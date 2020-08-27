29e62ac00b7d502605c8db66c3c8f312febc82f6
rawat boonchoo <rawat boonchoo.rawat@gmail.com>
2020-08-27 14:03:27 +0700

    create profile component

---
+++ vueproject/src/components/profile.vue
@@ -0,0 +1,19 @@
+  <!-- html -->
+<template>
+    <h1>
+        {{ msg }}
+    </h1>
+</template>
+
+<script>
+export default {
+    name:'profile',
+    data:function(){
+        msg:'This us my profile component'
+    }
+}
+</script>
+
+<style>
+
+</style>
